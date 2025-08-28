9d899d0f1d0a97a281d46596b7b93bf4902c6bcd
Mahesh <maheshpererakm@gmail.com>
2025-08-28 10:18:15 +0530

    fixing css for navbar and logo

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./NavBar.css";
 import NavLinks from "./NavLinks/NavLinks";
+import logo from "../../images/logo.png";
 
 function NavBar() {
   return (
     <>
       <header>
-        <div className="logo">Logo</div>
-        <div className="navbar">
-          <NavLinks linkname="Home" url="#home" />
-          <NavLinks linkname="About" url="#about" />
-          <NavLinks linkname="Contact" url="#contact" />
+        <div className="logoContainer">
+          <img src={logo} alt="logo" className="logo" />
+        </div>
+        <div className="rightColumn">
+          <div className="navbar">
+            <NavLinks linkname="Home" url="#home" />
+            <NavLinks linkname="About" url="#about" />
+            <NavLinks linkname="Contact" url="#contact" />
+          </div>
         </div>
       </header>
     </>
