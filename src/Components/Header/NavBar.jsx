260047b02bd19f88af9d002ab2280b42522f6eae
Mahesh <maheshpererakm@gmail.com>
2025-08-28 17:11:25 +0530

    header fixing

--- src/Components/Header/NavBar.jsx
+++ src/Components/Header/NavBar.jsx
@@ -7,22 +7,19 @@ function NavBar() {
   return (
     <>
       <header>
-        <div className="logoContainer">
-          <img src={logo} alt="logo" className="logo" />
-        </div>
-        {/* <div className="rightColumn">
-          <div className="navbar">
-            <NavLinks linkname="Home" url="#home" />
-            <NavLinks linkname="About" url="#about" />
-            <NavLinks linkname="Contact" url="#contact" />
+        <div className="header-bar">
+          <div className="logoContainer">
+            <img src={logo} alt="logo" className="logo" />
           </div>
-        </div> */}
-        <div className="search-bar">
-          <input type="text" placeholder="Search" />
-          <button className="searchBtn">Search</button>
-        </div>
-        <div className="login-signup">
-          <NavLinks linkname="Login/Sign Up" url="#login/sign up" />
+          <div className="search-bar">
+            <input type="text" placeholder="Search" />
+            <button className="searchBtn">Search</button>
+          </div>
+          <div className="login-signup">
+            <button className="login-signup-btn">
+              <NavLinks linkname="Login/Sign Up" url="#login/sign up" />
+            </button>
+          </div>
         </div>
       </header>
     </>
